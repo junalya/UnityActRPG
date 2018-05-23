57ca8944e6ccf3dd299d6919d5e7bcb8d061cb7c
junalya <sippuu1208@iclodu.com>
2018-05-23 17:23:44 +0900

    マスター版

---
+++ ACTRPG/Assets/Script/CameraContlole.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContlole : MonoBehaviour {
+
+    public GameObject Player;
+
+    private Vector3 DfCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ACTRPG/Assets/Script/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    //速度
+    public float speed = 7;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = -1;
+
+        //移動
+        Vector2 Cuthrhu = new Vector2(x,y).normalized;
+
+        GetComponent<Rigidbody2D>().velocity = Cuthrhu * speed;
+    }
+}
